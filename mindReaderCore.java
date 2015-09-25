de1d439927ad5ce417aaa1bc180a2b6f0fd72c98
bomb1e <ericgithinjimaina@gmail.com>
2015-09-25 10:20:22 +0000

    Fix Files

---
+++ mindReaderCore.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class mindReaderCore {
+	Scanner in = new Scanner(System.in);
+	Random rnd = new Random();
+
+	// Shuffles/Jumbles up an array of integers
+	public void jumbler(String[] array) {
+		for (int i = array.length - 1; i > 0 ; i--) {
+		 	int index = rnd.nextInt(i + 1);
+		 	String a = array[index];
+		 	array[index] = array[i];
+		 	array[i] = a;
+		}
+	}
+	
+	// Returns a random element from an array
+	public String randomElementPicker(String[] array) {
+		return array[rnd.nextInt(array.length)];
+	}
+	
+
+	public int coreGame(String[][] cardArray){
+		int sum = 0;
+		// jumble/shuffle up card
+		for (int i = 0; i <= cardArray.length; i++) {
+			// jumble/shuffle cardArray[i][0]
+			jumbler(cardArray[i]);
+			// Show Card to user
+			for (int j = 0; j <= cardArray[i].length; j++) {
+				System.out.print(cardArray[j][i] + " ");
+				if(j%2==0){
+					System.out.println("\n");
+				}	
+			}
+		System.out.println("Is your selection in this card?\n (y/n)");
+			if((in.nextLine()).equals("y")) {
+				// add card[1] to sum if 'Yes'
+				sum += Integer.parseInt(cardArray[i][0]);
+			}	
+		}
+		return sum;
+	}
+	
+}
